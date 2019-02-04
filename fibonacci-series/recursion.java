100b729d99e5bf23898716f2d1b6d9e2ec3c90ec
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-02-04 07:24:05 +0530

    Updated NthFibonacci function()

--- fibonacci-series/recursion.java
+++ fibonacci-series/recursion.java
@@ -7,6 +7,12 @@
 import java.util.*;
 
 public class recursion {
+    public static int nthFibonacci(int n) {
+        if (n <= 1)
+            return n;
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
     public static void main(String args[]) {
 
     }
